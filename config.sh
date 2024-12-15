b3464b4d7c828a1778ce5d7035069ed5cf5f8562
XiYang6666 <1782356858@qq.com>
2024-12-15 13:14:24 +0800

    :bug: fix config

--- config.sh
+++ config.sh
@@ -3,5 +3,5 @@ BUILD_QQNT_DATA_LIST=(
   # 3.1.2-12912,https://dldir1.qq.com/qqfile/qq/QQNT/80d33f88/linuxqq_3.1.2-12912
   # 3.2.5-21357,https://dldir1.qq.com/qqfile/qq/QQNT/7c0c5cc3/linuxqq_3.2.5-21357
   # 3.2.7-240428,https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.7_240428_amd64_01.deb
-  https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.15_241210_amd64_01.deb
+  3.2.15-241210,https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.15_241210_amd64_01.deb
 )
